95852e8df135cba363da5368d012d5d8a345a29f
Sheehan Olver <solver@mac.com>
2020-01-19 09:27:18 +0000

    Add Painleve I Heaven and Hell

--- ODEs/Newton iteration for Nonlinear BVPs.jl
+++ ODEs/Newton iteration for Nonlinear BVPs.jl
@@ -1,4 +1,4 @@
-using ApproxFun
+using ApproxFun, Plots
 
 x=Fun()
 u0=0.0x
@@ -10,3 +10,24 @@ x=Fun()
 u0=0.0x
 N=u->[u(-1.),u(1.),0.1u''+sin(u)-1.]
 u=newton(N,u0)
+
+
+##
+# Heaven and hell for PI
+##
+
+M = 10.0
+x = Fun(0..M)
+N = u -> [u(0), u(M)-sqrt(M), u'' - u^2 + x]
+u0 = 0.0x
+@time heaven = newton(N, u0)
+
+u0 = heaven-10x * exp(-x)
+@time hell = newton(N, u0)
+plot(heaven; label="heaven", linewidth=1.5, legend=:bottomright, title="Painleve I: Heaven and Hell")
+plot!(hell; label="hell", linewidth=1.5, linestyle=:dash)
+savefig("heavenhell.pdf")
+
+scatter(abs.(heaven.coefficients); yscale=:log10,  label="heaven", markersize=1.5, title="Painleve I: Heaven and Hell Coefficients")
+scatter!(abs.(hell.coefficients); yscale=:log10,  label="hell", markersize=1.5, marker=:square)
+savefig("heavenhellcoefficients.pdf")
