69f09963270fc9fcf3cd6772d86f320033442228
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-08 21:50:22 +0530

    linear ODEs

--- ODEs/Quantum_states.jl
+++ ODEs/Quantum_states.jl
@@ -8,7 +8,7 @@ B = [Dirichlet(S); continuity(S,0:1)]
 λ, v = ApproxFun.eigs(B, -D^2 + V, 500,tolerance=1E-10)
 
 p = plot(V; legend=false)
-    for k=1:20
-        plot!(real(v[k]/norm(v[k]) + λ[k]))
-    end
-    p
+for k=1:20
+    plot!(real(v[k]/norm(v[k]) + λ[k]))
+end
+p
